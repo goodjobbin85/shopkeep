10c0b81e90bf6d806cac4fdaf461544c0f10bdfa
Thomas Dubiel <td1729@hotmail.com>
2021-10-06 13:52:12 -0500

    Initialize repository

---
+++ config.ru
@@ -0,0 +1,3 @@
+require './config/application' 
+
+run RubyOnSails::Application.new
---
+++ config/application.rb
@@ -0,0 +1,6 @@
+require 'ruby_on_sails' 
+
+module ShopKeep 
+    class Application < RubyOnSails::Application 
+    end 
+end 
